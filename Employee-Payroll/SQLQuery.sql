ec06d48f44403c2885690d03aa4c2827cc305d7a
rinkuberde <rinkuberde@gmail.com>
2023-02-13 21:29:26 +0530

    [Rinku] -Create Employee-Payroll Table

--- Employee-Payroll/SQLQuery.sql
+++ Employee-Payroll/SQLQuery.sql
@@ -1,3 +1,12 @@
 ﻿-- Welcome to EmployeePayroll Problem
 --     UC-01-CreateDatabase
 create database Payroll 
+use Payroll
+-- UC-02 Create Employee_Payroll Table
+create Table EmployeePayroll
+(
+id int identity primary key,
+name varchar(200) not null,
+salary money not null,
+start_Date date not null
+)
