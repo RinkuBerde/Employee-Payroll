f9a86d334bd77a401ace51dba630d9d1c32182cc
rinkuberde <rinkuberde@gmail.com>
2023-02-16 17:28:59 +0530

    [Rinku] - Implement the ER Diagram

--- Employee-Payroll/SQLQuery.sql
+++ Employee-Payroll/SQLQuery.sql
@@ -68,3 +68,96 @@ select * from EmployeePayroll
 
 -- UC10 Adding duplicate names
 insert into  EmployeePayroll values('Rinku',20000,'2023-01-12','F','1234567891','India',null,'EXTC',10000,0,0,null)
+
+------- UC 11: Implement the ER Diagram into Payroll Service DB -------
+        --Create Table for Company---
+create table Company
+(
+CompanyID int identity(1,1) primary key,
+CompanyName varchar(100)
+
+   --Insert Values in Company table
+Insert into Company values 
+('Softech'),
+('Infotech')
+Select * from Company
+
+--Create Employee Table
+drop table EmployeePayroll
+create table Employee
+(
+EmployeeID int identity(1,1) primary key,
+CompanyIdentity int,
+EmployeeName varchar(200),
+EmployeePhoneNumber bigInt,
+EmployeeAddress varchar(200),
+StartDate date,
+Gender char,
+Foreign key (CompanyIdentity) references Company(CompanyID)
+)
+
+--Insert Values in Employee
+insert into Employee values
+(1,'Rinku Berde',964325436,'5298 Wild Indigo, Georgia,340002','2018-03-28','F'),
+(2,'Yukta Mane',6742905468,'Constitution Ave Fairfield, California(CA), 94533','2017-04-22','F'),
+(1,'Ketan Kumar',7878655050,'Bernard Shaw, Georgia,132001 ','2015-08-22','M'),
+(2,'Swayam Rane',7812905000,'Bernard Shaw, PB Marg Bareilly','2016-08-29','M')
+Select * from Employee
+
+    --Create Payroll Table--
+create table PayrollCalculate
+(
+BasicPay float,
+Deductions float,
+TaxablePay float,
+IncomeTax float,
+NetPay float,
+EmployeeIdentity int,
+Foreign key (EmployeeIdentity) references Employee(EmployeeID)
+)
+
+     --Insert Values in Payroll Table
+insert into PayrollCalculate(BasicPay,Deductions,IncomeTax,EmployeeIdentity) values 
+(4000000,1000000,20000,1),
+(4500000,200000,4000,2),
+(6000000,10000,5000,3),
+(9000000,399994,6784,4)
+
+select * from PayrollCalculate
+
+     --Update Derived attribute values 
+update PayrollCalculate set TaxablePay=BasicPay-Deductions
+
+update PayrollCalculate set NetPay=TaxablePay-IncomeTax
+select * from PayrollCalculate
+
+   --Create Department Table
+create table Department
+(
+DepartmentId int identity(1,1) primary key,
+DepartName varchar(100)
+)
+    --Insert Values in Department Table
+insert into Department values
+('Marketing'),
+('Sales'),
+('IT')
+select * from Department
+
+   --Create table EmployeeDepartment
+create table EmployeeDepartment
+(
+DepartmentIdentity int ,
+EmployeeIdentity int,
+Foreign key (EmployeeIdentity) references Employee(EmployeeID),
+Foreign key (DepartmentIdentity) references Department(DepartmentID)
+)
+
+   --Insert Values in EmployeeDepartment
+insert into EmployeeDepartment values
+(3,1),
+(2,2),
+(1,3),
+(3,4)
+
+select * from EmployeeDepartment
